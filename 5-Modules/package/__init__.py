b1d9e55e2937a9520ee401327a9a1970e62527ba
Jamadagni Kotamsetty <jamadagnikotamsetty@192.168.1.8>
2025-09-10 00:01:00 +0530

    added new code for creating and using packages/modules

---
+++ 5-Modules/package/__init__.py
@@ -0,0 +1,4 @@
+'''___init__.py is a special file used in python to define 
+to define package and initialize their namespaces
+'''
+
---
+++ 5-Modules/test.py
@@ -0,0 +1,5 @@
+from package.maths import *
+from package.subpackages.mult import multiply
+print(multiply(4,5))
+print(addition(2,3))
+print(substraction(4,3))
